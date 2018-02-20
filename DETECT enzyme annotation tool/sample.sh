43b253e851cb791835e2b127655f3dc44e232dc3
ParkinsonLab <ParkinsonLab>
2018-02-20 16:32:47 -0500

    Changed README for enzyme annotation tool

--- DETECT enzyme annotation tool/sample.sh
+++ DETECT enzyme annotation tool/sample.sh
@@ -1,6 +1,7 @@
-DETECT_TOOL=/absolute/file/path/to/detect_folder/detect.py
+DETECT_FOLDER=/absolute/file/path/to/detect_folder
+DETECT_TOOL=${DETECT_FOLDER}/detect.py
 
-export PATH=/absolute/file/path/to/detect_folder/:$PATH # add the path to the folder containing DETECT
+export PATH=${DETECT_FOLDER}/:$PATH # add the path to the folder containing DETECT
 export PATH=/usr/local/bin/:$PATH # add the path to emboss to PATH
 
 python $DETECT_TOOL sample.fasta --output_file sample.out --top_predictions_file sample.top --num_threads 8 --fbeta_file sample.f1.out --beta 1
