7cf3d649eaa894f59584d057f4394927ab35b4c1
ParkinsonLab <ParkinsonLab>
2018-03-09 13:36:16 -0500

    Added double ampersand

--- Pipeline for creating DETECT db using SciNet/0_run_pipeline_for_one_ec.sh
+++ Pipeline for creating DETECT db using SciNet/0_run_pipeline_for_one_ec.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-python 1_create_fasta_with_target_ec_from_dat.py $1;
-blastp -query $1.fasta -db uniprot_sprot_all_blastdb -outfmt "7 qseqid sseqid ssac qstart qend sstart send evalue bitscore qcovs qcovhsp slen pident" -out $1.bl -evalue 1 -max_target_seqs 100000;
-python 3_get_blast_results_unique_pairs.py $1;
-python 4_needleall_one_ec.py $1 $2;
-cat *.$2.needleall > $1.needleall.final;
-rm *.$2.query.fasta;
-rm *.$2.hits.fasta;
-rm *.error;
-rm *.$2.needleall;
-python 5_format_needleall_one_ec.py $1;
-python 6_generate_probability_profile_for_one_ec.py $1 $2;
-rm $1.*;
-rm $1_*;
+python 1_create_fasta_with_target_ec_from_dat.py $1 &&
+blastp -query $1.fasta -db uniprot_sprot_all_blastdb -outfmt "7 qseqid sseqid ssac qstart qend sstart send evalue bitscore qcovs qcovhsp slen pident" -out $1.bl -evalue 1 -max_target_seqs 100000 &&
+python 3_get_blast_results_unique_pairs.py $1 &&
+python 4_needleall_one_ec.py $1 $2 &&
+cat *.$2.needleall > $1.needleall.final &&
+rm *.$2.query.fasta &&
+rm *.$2.hits.fasta &&
+rm *.error &&
+rm *.$2.needleall &&
+python 5_format_needleall_one_ec.py $1 &&
+python 6_generate_probability_profile_for_one_ec.py $1 $2 &&
+rm $1.* &&
+rm $1_* &&
 wait
